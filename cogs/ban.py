9cc561102079ad8b1838ab8669ff73ded495df56
Navitank <halocore099@gmail.com>
2024-05-10 17:55:06 +0800

    Create ban.py

---
+++ cogs/ban.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class ban(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f"{member} has been banned!")
+
+async def setup(bot):
+    await bot.add_cog(ban(bot))
